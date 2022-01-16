c418d09fa7e90e2d7fd35388308d03093f3c3e52
Bakti Apriliansyah <apriliansyah.bakti@gmail.com>
2022-01-16 13:36:56 +0700

    project initialization

---
+++ src/main/java/com/bakti/tictactoe/tictactoe/App.java
@@ -0,0 +1,13 @@
+package com.bakti.tictactoe.tictactoe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
